9cf8f92568937703888b9dfdffbf4cf803f9c486
xulianghui1 <twomeetings@gmail.com>
2017-07-24 21:23:47 +0800

    complete sync_shell.sh

--- sync_shell.sh
+++ sync_shell.sh
@@ -2,13 +2,13 @@
 lastCommit=`git rev-parse HEAD`
 while [ 1 ]
 do
-    echo 'shell start...';
+    echo 'while start...';
 		git fetch;
 		currCommit=`git rev-parse HEAD`;
 		if [ $lastCommit != $currCommit ]
 		then
 			lastCommit=`git rev-parse HEAD`;
-			echo 'start push mirror';
+			echo 'start push mirror...';
 			git push --mirror https://git.oschina.net/xulianghui1/mirror-push-test.git;
 		fi
     sleep 6
