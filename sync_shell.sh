ea7dd8942dee279ee1ddc9b837f3a1c613a365e9
xulianghui1 <twomeetings@gmail.com>
2017-07-24 21:16:10 +0800

    add sync_shell for testing

---
+++ sync_shell.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+lastCommit=`git rev-parse HEAD`
+while [ 1 ]
+do
+    echo 'shell start...';
+		git fetch;
+		currCommit=`git rev-parse HEAD`;
+		if [ $lastCommit != $currCommit ]
+		then
+			lastCommit=`git rev-parse HEAD`;
+			echo 'start push mirror';
+			git push --mirror https://git.oschina.net/xulianghui1/mirror-push-test.git;
+		fi
+    sleep 6
+done
